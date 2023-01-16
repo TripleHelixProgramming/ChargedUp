72e25bd6b8e01188f0bb5634ea7a6407babaa0d0
Joshua Nichols <45806130+JoshuaAN@users.noreply.github.com>
2023-01-16 09:31:01 -0500

    Re-add DriveTrajectory

--- src/main/include/commands/DriveTrajectory.h
+++ src/main/include/commands/DriveTrajectory.h
@@ -1,28 +1,28 @@
-// #pragma once
+#pragma once
 
-// #include <frc2/command/CommandBase.h>
-// #include <frc2/command/CommandHelper.h>
-// #include <frc/Timer.h>
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc/Timer.h>
 
-// #include "subsystems/SwerveDrive.h"
-// #include "util/Trajectory.h"
+#include "subsystems/SwerveDrive.h"
+#include "util/Trajectory.h"
 
-// class DriveTrajectory
-//     : public frc2::CommandHelper<frc2::CommandBase, DriveTrajectory> {
-//  public:
-//    DriveTrajectory(SwerveDrive* drive, Trajectory& trajectory);
+class DriveTrajectory
+    : public frc2::CommandHelper<frc2::CommandBase, DriveTrajectory> {
+ public:
+   DriveTrajectory(SwerveDrive* drive, Trajectory& trajectory);
 
-//   void Initialize() override;
+  void Initialize() override;
 
-//   void Execute() override;
+  void Execute() override;
 
-//   void End(bool interrupted) override;
+  void End(bool interrupted) override;
 
-//   bool IsFinished() override;
-//  private:
-//   SwerveDrive* m_drive;
+  bool IsFinished() override;
+ private:
+  SwerveDrive* m_drive;
 
-//   Trajectory m_trajectory;
+  Trajectory m_trajectory;
 
-//   frc::Timer m_timer;
-// };
+  frc::Timer m_timer;
+};
