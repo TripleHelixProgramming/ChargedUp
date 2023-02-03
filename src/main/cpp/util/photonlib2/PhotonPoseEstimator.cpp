ff9c9264373608a855c8df02fb55f68684870783
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-02-02 22:01:14 -0500

    Work Feb 2

--- src/main/cpp/util/photonlib2/PhotonPoseEstimator.cpp
+++ src/main/cpp/util/photonlib2/PhotonPoseEstimator.cpp
@@ -11,6 +11,7 @@
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core/mat.hpp>
 #include "frc/apriltag/AprilTag.h"
+#include "frc/smartdashboard/SmartDashboard.h"
 #include "units/angle.h"
 #include <wpi/SmallVector.h>
 
@@ -24,6 +25,8 @@
 #include <photonlib/PhotonCamera.h>
 #include <photonlib/PhotonPipelineResult.h>
 #include <photonlib/PhotonTrackedTarget.h>
+
+#include <chrono>
 
 using namespace frc;
 using namespace units;
@@ -43,6 +46,7 @@ PhotonPoseEstimator::PhotonPoseEstimator(frc::AprilTagFieldLayout tags,
       m_referencePose(frc::Pose3d()) {}
 
 std::optional<EstimatedRobotPose> PhotonPoseEstimator::Update() {
+
   auto result = m_camera.GetLatestResult();
 
   if (!result.HasTargets()) {
@@ -56,7 +60,7 @@ std::optional<EstimatedRobotPose> PhotonPoseEstimator::Update() {
   std::vector<cv::Point2f> imagePoints;
   
   // Add all target corners to main list of corners
-  auto tagCornerToObjectPoint = [](Translation3d corner, Pose3d tagPose) {
+  constexpr auto tagCornerToObjectPoint = [](Translation3d corner, Pose3d tagPose) {
     Translation3d point = corner.RotateBy(tagPose.Rotation()) + tagPose.Translation();
     return cv::Point3f(point.X().value(), point.Y().value(), point.Z().value());
   };
@@ -69,20 +73,23 @@ std::optional<EstimatedRobotPose> PhotonPoseEstimator::Update() {
       for (auto& corner : targetCorners) {
         imagePoints.emplace_back(corner.first, corner.second);
       }
-      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(-3_in, -3_in, 0_m), tagPose.value()));
-      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(+3_in, -3_in, 0_m), tagPose.value()));
-      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(+3_in, +3_in, 0_m), tagPose.value()));
       objectPoints.push_back(tagCornerToObjectPoint(Translation3d(-3_in, +3_in, 0_m), tagPose.value()));
+      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(+3_in, +3_in, 0_m), tagPose.value()));
+      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(+3_in, -3_in, 0_m), tagPose.value()));
+      objectPoints.push_back(tagCornerToObjectPoint(Translation3d(-3_in, -3_in, 0_m), tagPose.value()));
     }
   }
 
+  SmartDashboard::PutNumber("Number of corners", imagePoints.size());
   if (imagePoints.empty()) {
     return std::nullopt;
   }
 
-  // Use SQPnP
+  // Use OpenCV ITERATIVE solver
   cv::Mat rvec(3, 1, cv::DataType<double>::type);
   cv::Mat tvec(3, 1, cv::DataType<double>::type);
+
+  auto begin = std::chrono::system_clock::now();
 
   cv::solvePnP(objectPoints, 
                imagePoints, 
@@ -90,17 +97,29 @@ std::optional<EstimatedRobotPose> PhotonPoseEstimator::Update() {
                m_distortionCoefficients, 
                rvec, 
                tvec, 
-               cv::SOLVEPNP_SQPNP);
+               cv::SOLVEPNP_ITERATIVE);
 
   Vectord<3> rotationVector;
-  rotationVector[0] = rvec.at<double>(0, 0); 
-  rotationVector[1] = rvec.at<double>(1, 0); 
-  rotationVector[2] = rvec.at<double>(2, 0); 
+  rotationVector[0] = rvec.at<double>(2, 0);
+  rotationVector[1] = rvec.at<double>(0, 0); 
+  rotationVector[2] = rvec.at<double>(1, 0); 
 
-  return EstimatedRobotPose(Pose3d(Translation3d(meter_t{tvec.at<double>(0, 0)},
-                                                 meter_t{tvec.at<double>(1, 0)},
-                                                 meter_t{tvec.at<double>(2, 0)}), 
-                                   Rotation3d(rotationVector, radian_t{rotationVector.norm()})), 
+  Pose3d pose{Translation3d(meter_t{tvec.at<double>(2, 0)},
+                            meter_t{-tvec.at<double>(0, 0)},
+                            meter_t{tvec.at<double>(1, 0)}), 
+              Rotation3d(rotationVector, radian_t{rotationVector.norm()})};
+
+  auto end = std::chrono::system_clock::now();
+
+  SmartDashboard::PutNumber("SQPNP/Translation/X", pose.X().value());
+  SmartDashboard::PutNumber("SQPNP/Translation/Y", pose.Y().value());
+  SmartDashboard::PutNumber("SQPNP/Translation/Z", pose.Z().value());
+  SmartDashboard::PutNumber("SQPNP/Rotation/Yaw", pose.Rotation().Z().convert<degree>().value());
+  SmartDashboard::PutNumber("SQPNP/Rotation/Pitch", pose.Rotation().Y().convert<degree>().value());
+  SmartDashboard::PutNumber("SQPNP/Rotation/Roll", pose.Rotation().X().convert<degree>().value());
+  SmartDashboard::PutNumber("SQPNP/Time", std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+
+  return EstimatedRobotPose(pose.TransformBy(m_robotToCamera.Inverse()),
                             result.GetTimestamp());
 }
 
