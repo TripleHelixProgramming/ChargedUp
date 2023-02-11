2ed60e02b3c7109b569d756289f4022bcef75ee0
Joshua Nichols <45806130+JoshuaAN@users.noreply.github.com>
2023-02-10 21:28:06 -0500

    Tests failing locally

--- src/test/cpp/FactorGraphTest.cpp
+++ src/test/cpp/FactorGraphTest.cpp
@@ -1,21 +1,21 @@
 #include <gtest/gtest.h>
 
-// #include <Eigen/Core>
+#include <Eigen/Core>
 // #include "util/graph/FactorGraphSolver.hpp"
 
 // Eigen::VectorXd reprojection(Eigen::Vector3d T, Eigen::Matrix3d R) {
 //   return R * Eigen::Vector3d::Ones();
 // }
 
-// TEST(FactorGraphTest, Translation) {
-//   std::cout << "Factor graph" << std::endl;
+TEST(FactorGraphTest, Translation) {
+  std::cout << "Factor graph" << std::endl;
 
-//   Eigen::VectorXd measurement;
-//   measurement << 1, 1, 1;
+  Eigen::VectorXd measurement;
+  measurement << 1, 1, 1;
 
 //   std::function<Eigen::VectorXd(Eigen::Vector3d, Eigen::Matrix3d)> h = reprojection;
 
 //   graph::OptimizePose(h, measurement);
 
 //   EXPECT_EQ(0.0, 0.0);
-// }
+}
